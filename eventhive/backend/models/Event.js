eb4d53694a821c316e607d8ca0f1da4e629f7c8f
Apotts00 <apotts00@email.cpcc.edu>
2025-05-04 17:29:22 -0400

    Create Event.js

---
+++ eventhive/backend/models/Event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  checklist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ChecklistItem' }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+module.exports = Event;
