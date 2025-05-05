c62fbb6a331a46108ee0d5983266694ca91cdc89
Apotts00 <apotts00@email.cpcc.edu>
2025-05-04 20:31:38 -0400

    Update LoginPage.js

--- eventhive/frontend/src/pages/LoginPage.js
+++ eventhive/frontend/src/pages/LoginPage.js
@@ -25,24 +25,25 @@ const LoginPage = () => {
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
+  <input
+    type="email"
+    placeholder="Email"
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    autoComplete="email"
+  />
+  <input
+    type="password"
+    placeholder="Password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    autoComplete="current-password"
+  />
+  {error && <p>{error}</p>}
+  <button type="submit">Login</button>
+</form>
+<p>Don't have an account? <Link to="/register">Register here</Link></p>
 
-      {/* Wrap the "Don't have an account?" part with the rest */}
-      <p>Don't have an account? <Link to="/register">Register here</Link></p>
     </div>
   );
 };
