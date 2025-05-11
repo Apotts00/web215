2125e367a18b1cd35d9d6e59d5b9f45f1c47a849
Apotts00 <apotts00@email.cpcc.edu>
2025-05-11 19:29:46 -0400

    Update Dashboard.js

--- eventhive/eventhive-frontend/src/pages/Dashboard.js
+++ eventhive/eventhive-frontend/src/pages/Dashboard.js
@@ -21,7 +21,7 @@ const Dashboard = () => {
       navigate('/login');
       return;
     }
-    const response = await axios.get(`${API_URL}/api/dashboard`, {
+    const response = await axios.get(`$eventhive-55x2.onrender.com/api/dashboard`, {
   headers: { Authorization: `Bearer ${token}` },
 });
     setEvents(response.data);
