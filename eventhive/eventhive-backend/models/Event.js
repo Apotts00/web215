79ea2ce3df9f197295466798daae32ddab64e0b6
Apotts00 <apotts00@email.cpcc.edu>
2025-05-12 00:22:08 -0400

    Update Event.js

--- eventhive/eventhive-backend/models/Event.js
+++ eventhive/eventhive-backend/models/Event.js
@@ -4,6 +4,7 @@ const EventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   description: String,
+  location: String,
   checklist: [{ item: String, completed: Boolean }],
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
