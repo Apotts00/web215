9f1ece411decce3123a9975c3f361fe174b62282
Apotts00 <apotts00@email.cpcc.edu>
2025-05-11 21:27:08 -0400

    Update Register.js

--- eventhive/eventhive-frontend/src/pages/Register.js
+++ eventhive/eventhive-frontend/src/pages/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import './Register.css';
+import './Login.css';
 
 const Register = () => {
   const [email, setEmail] = useState('');
