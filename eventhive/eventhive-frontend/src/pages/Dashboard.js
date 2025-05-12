83d368492b78dbbd051409ecebe6f8441bbd9879
Apotts00 <apotts00@email.cpcc.edu>
2025-05-11 23:47:36 -0400

    Update Dashboard.js

--- eventhive/eventhive-frontend/src/pages/Dashboard.js
+++ eventhive/eventhive-frontend/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Dashboard.css';
+import Header from '../components/Header';
 
 const Dashboard = () => {
   const [events, setEvents] = useState([]);
